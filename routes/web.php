77ab40e887881d7b4518bcf1aa78e6cf3c33d08e
FajarSidiq08 <sidiqfajar491@gmail.com>
2025-01-17 11:12:36 +0700

    menambahkan dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Back\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index']);
