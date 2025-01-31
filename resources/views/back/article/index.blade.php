ece73a4769df0ef0f7ae8cfe3d5451c853398e6d
FajarSidiq08 <sidiqfajar491@gmail.com>
2025-01-31 13:21:50 +0700

    menambahkan link pada sidebar

---
+++ resources/views/back/article/index.blade.php
@@ -0,0 +1,51 @@
+@extends('back.layout.template')
+
+@section('content')
+    {{-- content --}}
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Articles</h1>
+        </div>
+
+        <div class="mt-3">
+            <button class="btn btn-success mb-2" data-bs-toggle="modal" data-bs-target="#modalCreate">Create</button>
+
+            @if ($errors->any())
+                <div class="my-3">
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endif
+
+            @if (session('success'))
+                <div class="my-3">
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                </div>
+            @endif
+
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Slug</th>
+                        <th>Created At</th>
+                        <th>Function</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+
+    </main>
+@endsection
