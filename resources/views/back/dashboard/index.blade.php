377ed053b8891e773048253fd5d340150dfa3490
FajarSidiq08 <sidiqfajar491@gmail.com>
2025-02-07 14:53:37 +0700

    menambahkan latest dan popular articles

--- resources/views/back/dashboard/index.blade.php
+++ resources/views/back/dashboard/index.blade.php
@@ -7,19 +7,85 @@
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2">Dashboard</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-                <div class="btn-group me-2">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="card text-bg-primary mb-3" style="max-width: 100%;">
+                    <div class="card-header">Total Articles</div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $total_articles }} Articles</h5>
+                        <p class="card-text">
+                            <a href="{{ url('articles') }}" class="text-white">View</a>
+                        </p>
+                    </div>
                 </div>
-                <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                    <span data-feather="calendar" class="align-text-bottom"></span>
-                    This week
-                </button>
+            </div>
+            <div class="col-6">
+                <div class="card text-bg-primary mb-3" style="max-width: 100%;">
+                    <div class="card-header">Total Categories</div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $total_categories }} Categories</h5>
+                        <p class="card-text">
+                            <a href="{{ url('categories') }}" class="text-white">View</a>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
 
-        <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+        <div class="row">
+            <div class="col-6">
+                <h4>Latest Articles</h4>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <th>No</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($latest_articles as $item)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $item->title }}</td>
+                            <td>{{ $item->Category->name }}</td>
+                            <td>{{ $item->created_at }}</td>
+                            <td class="text-center">
+                                <a href="{{ url('article/'. $item->id) }}"" class="btn btn-sm btn-secondary">Detail</a>
+                            </td>
+                        </tr>
+                            @endforeach
+                    </tbody>
+                </table>
+            </div>
 
+            <div class="col-6">
+                <h4>Popular Articles</h4>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <th>No</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($popular_articles as $item)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $item->title }}</td>
+                            <td>{{ $item->Category->name }}</td>
+                            <td>{{ $item->created_at }}</td>
+                            <td class="text-center">
+                                <a href="{{ url('article/'. $item->id) }}"" class="btn btn-sm btn-secondary">Detail</a>
+                            </td>
+                        </tr>
+                            @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </main>
 @endsection
