03cbca581fcb96e767974fcfc3fbeea92ea60fcd
FajarSidiq08 <sidiqfajar491@gmail.com>
2025-02-13 22:04:17 +0700

    fix bug (private dapat ditampilkan)

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
         }
 
         return view('front.home.index', [
-            'latest_post' => Article::latest()->first(),
+            'latest_post' => Article::latest()->whereStatus(1)->first(),
             'articles' => $articles,
             'categories' => Category::latest()->get(),
         ]);
